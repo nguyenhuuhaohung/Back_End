2020e339e7ce7203b43903b1899a5a8cab668721
nguyenhuuhaohung <nguyenhaohung640@gmail.com>
2025-02-26 21:50:06 +0700

    create file

--- Be1/form_update_author.php
+++ Be1/form_update_author.php
@@ -74,12 +74,7 @@
         </ul>
     </div>
     <!--start-top-serch-->
-    <div id="search">
-        <form action="result.html" method="get">
-            <input name="keyword" type="text" placeholder="Search here..." />
-            <button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white"></i></button>
-        </form>
-    </div>
+    <?php require_once "start_top_serch.php" ?>
     <!--close-top-serch-->
     <!--sidebar-menu-->
     <?php require_once 'sidebar_menu.php'; ?>
---
+++ Be1/start_top_serch.php
@@ -0,0 +1,6 @@
+<div id="search">
+        <form action="result.html" method="get">
+            <input name="keyword" type="text" placeholder="Search here..." />
+            <button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white"></i></button>
+        </form>
+    </div>
